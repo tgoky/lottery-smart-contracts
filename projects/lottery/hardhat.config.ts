476681ab03b4b7e8f05e44b5b649cc446b7b74df
tgoky <tgokyicloud6@gmail.com>
2022-12-03 23:22:10 +0100

    syrup & pancakes

---
+++ projects/lottery/hardhat.config.ts
@@ -0,0 +1,103 @@
+import type { HardhatUserConfig, NetworkUserConfig } from "hardhat/types";
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-web3";
+import "@nomiclabs/hardhat-truffle5";
+import "hardhat-abi-exporter";
+import "hardhat-contract-sizer";
+import "solidity-coverage";
+import "dotenv/config";
+
+const bscTestnet: NetworkUserConfig = {
+  url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+  chainId: 97,
+  accounts: ["1db0ee1d9afd74ed3f7bc71e302f4d96c15fcc191bd0b1af133dd28641028f76"],
+};
+
+const bscMainnet: NetworkUserConfig = {
+  url: "https://bsc-dataseed.binance.org/",
+  chainId: 56,
+  accounts: [process.env.KEY_MAINNET!],
+};
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      gas: 120000000,
+      blockGasLimit: 0x1fffffffffffff,
+    },
+     testnet: bscTestnet,
+    // mainnet: bscMainnet,
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.4",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
+        version: "0.6.2",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
+        version: "0.7.5",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
+        version: "0.6.0",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
+        version: "0.6.12",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+      {
+        version: "0.8.0",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+  abiExporter: {
+    path: "./data/abi",
+    clear: true,
+    flat: false,
+  },
+};
+
+export default config;
